838474941fa76cb900e4fa8664cc8ad58e4cfec3
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-12-07 19:52:06 +0000

    added two more pictres

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@ $(print)
 
       
 function print(){
-  var names = ["alastair", "ben", "guillame", "guus", "kwakes","marika","ollie","sarah","sed","calum"]
+  var names = ["alastair", "ben", "guillame", "guus", "kwakes","marika","ollie","sarah","sed","calum","marty","emily","pritpal"]
   for(var i=0;i<names.length;i++){
     $("#pictures").append("<li><img src='http://localhost:3000/" + names[i] + "'><div class='caption center-align'><h3>The Hassifier</h3><h5 class='light grey-text text-lighten-3'>Like you, but better.</h5></div></li>")
   }
