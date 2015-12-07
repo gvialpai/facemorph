d33706dece2a8778add66d122d0c7a1e31a3ff19
martyjg <martinjosephgormley@gmail.com>
2015-12-07 17:13:51 +0000

    Fixed error on main element

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,9 +5,9 @@ function init(){
   var pathname = window.location.pathname;
 
   console.log(pathname)
-getUrl(pathname+"guus")
-function getUrl(name){
-$.get("http://localhost:3000/facemorphs"+name, function(data){
+  getUrl(pathname+"guus")
+  function getUrl(name){
+    $.get("http://localhost:8000/facemorphs" + name, function(data) {
   // var url = data.url
   // console.log(data.url)
   // return data.url
@@ -16,9 +16,10 @@ $.get("http://localhost:3000/facemorphs"+name, function(data){
 // }
 
 function appendImage(url){
+  console.log("this is the append image thing")
   // var url = data.url
   console.log(url)
-  $( ".main" ).append( "<img src='"+url+"'>" );
+  $("main").append( "<img src='"+url+"'>" );
 }
 }
 }
